a72169aab5412bd141bf8215eb14e1e528875033
Kenji Inukai <kenji.inki@gmail.com>
2020-03-30 11:59:21 +0900

    Update travis_package_make.bash

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -5,6 +5,7 @@ sudo apt install opencv-data libopencv-dev python-opencv
 
 sudo apt install ros-melodic-cv-bridge
 #sudo apt install ros-melodic-cv-camera
+sudo apt install ros-melodic-image-transport-plugins 
 
 #sync
 rsync -av ./ ~/catkin_ws/src/pimouse_vision_control/
